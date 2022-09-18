76ee6ff782852b4f56115578bc82b6716db95677
Juliana Morikoshi <julianamorikoshi@gmail.com>
2022-09-18 19:09:57 -0300

    feat(web): new add dialog layout

--- web/src/components/CreateAdBanner.tsx
+++ web/src/components/CreateAdBanner.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { MagnifyingGlassPlus } from 'phosphor-react'
+import * as Dialog from '@radix-ui/react-dialog'
 
 export const CreateAdBanner: React.FC = () => {
   return (
@@ -14,10 +15,10 @@ export const CreateAdBanner: React.FC = () => {
           </span>
         </div>
 
-        <button className="py-3 px-4 bg-violet-500 text-white rounded hover:bg-violet-600 transition-colors flex items-center gap-3">
+        <Dialog.Trigger className="py-3 px-4 bg-violet-500 text-white rounded hover:bg-violet-600 transition-colors flex items-center gap-3">
           <MagnifyingGlassPlus size="24" />
           Publicar anúncio
-        </button>
+        </Dialog.Trigger>
       </div>
     </div>
   )
